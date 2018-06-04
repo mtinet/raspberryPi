e5b2c9f8e6dc2c65a1542691f19d23686f212640
Jason Juhyun Kim <mtinet@hanmail.net>
2018-06-04 11:37:41 +0900

    Delete js8.js

--- js8.js
+++
@@ -1,7 +0,0 @@
-var str = 'Good day to study about javascript';
-
-console.log(str.indexOf('study'));
-console.log(str.charAt(5));
-console.log(str.substr(5, 3));
-console.log(str.substring(5, 8));
-console.log(str.split(' '));
